535c52eadf930d1f8104c299a421785e5e03af7d
dustin-longenecker <dl-dev@outlook.com>
2018-10-15 10:11:56 -0600

    refactor navigation

--- client/src/components/Nav_Bar/index.js
+++ client/src/components/Nav_Bar/index.js
@@ -24,21 +24,18 @@ export default class NavBar extends React.Component {
       <div>
         <Navbar color="faded" light id="navbarFormat">
           <NavbarBrand href="/" className="mr-auto">WikiBoots Logo</NavbarBrand>
+          <input className="" type="search" placeholder="Search"></input>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse isOpen={!this.state.collapsed} navbar>
             <Nav navbar>
               <NavItem>
-              <Navigation/>
-              </NavItem>
-              <NavItem>
-                <NavLink href={routes.LANDING}>Search Lessons</NavLink>
-              </NavItem>
-              <NavItem>
                 <NavLink href={routes.LANDING}>Browse All Lessons</NavLink>
               </NavItem>
-
               <NavItem>
                 <NavLink href="/components/">About WikiBoots</NavLink>
+              </NavItem>
+              <NavItem>
+              <Navigation/>
               </NavItem>
             </Nav>
           </Collapse>
